dfd19b065f767f306d808150f8e6d94683ac5068
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-02-01 11:10:57 +0800

    add logs to connection class

--- src/lib/Connection.ts
+++ src/lib/Connection.ts
@@ -1,10 +1,13 @@
 import { driver, auth, type Driver, type Session } from "neo4j-driver";
 import type { Neo4jConfiguration } from "neo4u";
+import { Logger } from "./Logger";
 const { basic } = auth;
 
 export class Connection {
     private driver: Driver;
     private session: Session;
+    private uri: string;
+    private logger = Logger.getLogger("connection");
 
     getUri(config: Neo4jConfiguration): string {
         const { protocol, host, port } = config;
@@ -12,10 +15,17 @@ export class Connection {
     }
 
     constructor(config: Neo4jConfiguration) {
-        const uri = this.getUri(config);
+        this.uri = this.getUri(config);
         const { user, password } = config;
-        this.driver = driver(uri, basic(user, password));
-        this.session = this.driver.session();
+        this.logger.info(`establishing connection to ${this.uri}`);
+        try {
+            this.driver = driver(this.uri, basic(user, password));
+            this.session = this.driver.session();
+        } catch (error) {
+            this.logger.error(`error connecting to ${this.uri}`, error);
+            throw error;
+        }
+        this.logger.info(`successfully connected to ${this.uri}`);
     }
 
     getSession(): any {
@@ -23,7 +33,12 @@ export class Connection {
     }
 
     close(): void {
-        this.session.close();
-        this.driver.close();
+        try {
+            this.logger.info(`closing connection to ${this.uri}`);
+            this.session.close();
+            this.driver.close();
+        } catch (error) {
+            this.logger.error(`error closing connection to ${this.uri}`);
+        }
     }
 }
--- src/lib/Neo4u.ts
+++ src/lib/Neo4u.ts
@@ -1,15 +1,12 @@
 import { type Configuration } from "neo4u";
 import { Connection } from "./Connection";
-import { Logger } from "./Logger";
 
 export class Neo4u {
     private connection: Connection;
-    private logger: Logger;
 
     constructor(private config: Configuration) {
         const { neo4j } = this.config;
         this.connection = new Connection(neo4j);
-        this.logger = Logger.getLogger("neo4u")
     }
 
     getSession() {
