8713a8cf2990f5771250441f447267841c737dc1
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-02-03 11:23:24 +0800

    add documentation for query

--- src/lib/Query.ts
+++ src/lib/Query.ts
@@ -1,6 +1,9 @@
 import type { Session } from 'neo4j-driver';
 import { Logger } from './Logger';
 
+/**
+ * Class representing a database query.
+ */
 export class Query {
     private query: string;
     private parameters?: Record<string, any>;
@@ -8,6 +11,12 @@ export class Query {
     private logger = Logger.getLogger('query');
     private result: any;
 
+    /**
+     * Create a new Query instance.
+     * @param {Session} session - The session to use for the query.
+     * @param {string} query - The query string.
+     * @param {Record<string, any>} parameters - The parameters for the query.
+     */
     constructor(
         session: Session,
         query: string,
@@ -18,7 +27,10 @@ export class Query {
         this.parameters = parameters;
     }
 
-    async run() {
+    /**
+     * Run the query.
+     */
+    async run(): Promise<void> {
         try {
             this.logger.info(`Running query: ${this.query}`);
             this.result = await this.session.run(this.query, this.parameters);
@@ -28,7 +40,11 @@ export class Query {
         }
     }
 
-    getResult() {
+    /**
+     * Get the result of the query.
+     * @returns {any} The result of the query.
+     */
+    getResult(): any {
         return this.result;
     }
 }
